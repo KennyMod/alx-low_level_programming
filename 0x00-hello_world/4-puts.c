56d2780eb6b2e883261a2f9a92a10cbf583ce66a
Kenechukwu Modebelu <kmodebelu@gmail.com>
2023-02-16 06:45:15 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with a put function in it
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programmming is like building a multilingual puzzle");
+	return (0);
+}
