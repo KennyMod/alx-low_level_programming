96ac2b608384b5195711eb987736df2035f9d5af
Kenechukwu Modebelu <kmodebelu@gmail.com>
2023-02-16 22:17:53 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
-#include <stdio.h>                                                                  
-/**                                                                                 
- *  * Description: main - Prints out a string to stdout.                            
- *   * Return: 0 if success.                                                        
-*/                                                                                  
-int main(void)                                                                      
-{                                                                                   
-printf("with proper grammar, but the outcome is a piece of art,\n");                
-return (0);
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but thhe outcome is a piece of art,\n");
+	return (0);
+}
